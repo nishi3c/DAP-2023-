bfdae6e0f83031dc4ca371df87c38ecb7007bcd4
nishi3c <147357928+nishi3c@users.noreply.github.com>
2023-10-11 01:49:25 -0400

    Add files via upload

---
+++ DAP Demography.R
@@ -0,0 +1,15 @@
+2+2
+log(2)
+log2 <- log(2) #comment example# 
+# comments created by putting # in front 
+
+#Step1: create a working directory 
+#Step2: download files from DAP into the directory 
+#Step3: Decide variables needed & Data Linkage --> taking variables needed from file and create table 
+#Step4: Data Quality --> making inquires and figures of the data, figuring out potential problems and useibilty 
+#Step5: Figure out what questions can be answered; may need to add more variable 
+#Step6: Constructing lifetables 
+#Step7: Using lifetable and other data to answer questions 
+
+
+#download heaven package and then able to use read_sav 
